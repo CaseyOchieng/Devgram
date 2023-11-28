38971c51adbdfeebdc25b3ddfa30fd7cfc62c9a6
casey <caseyochieng46@gmail.com>
2023-11-28 20:32:14 +0300

    added assests

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import './css/globals.css';
+function App() {
+  return (
+    <main className='flex h-screen'>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
